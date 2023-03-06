bb70cbf3819942efc449fb2f8d09f72240210f1a
Dorian Terbah <dorian.terb@gmail.com>
2023-03-06 15:55:16 +0100

    Update pmugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,10 +1,14 @@
 import { SketchComponentFactory } from "./factory";
 import SketchComponent from "./sketch-component";
-import { GenericSketchComponentClass } from "./types";
+import { Class, GenericSketchComponentClass } from "./types";
 import { Component } from 'vue';
 
 export interface KonectPlugin {
     components: Array<GenericSketchComponentClass>;
-    factories: Array<SketchComponentFactory<SketchComponent<unknown>>>;
+    factories: Array<Class<SketchComponentFactory<SketchComponent<unknown>>>>;
     popup: Map<GenericSketchComponentClass, Component>;
+    pluginInformation: {
+        name: string;
+        description: string;
+    }
 }
